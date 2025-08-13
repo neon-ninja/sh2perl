a6dd95aef4df8d944bc22a6a3892fc3139ca6d00
gmatht <gmatht@gmail.com>
2025-08-13 17:45:11 +1200

    Stop testing ls -a as not worry about permissions which are OS dependant for now

--- examples/simple_backup.sh
+++ examples/simple_backup.sh
@@ -2,5 +2,8 @@
 
 # Simple shell script example
 echo "Hello, World!"
-ls -la
-grep "pattern" file.txt 
+ls
+echo `ls`
+#Lets not consider ls -la at the moment as permissions are OS dependent
+#ls -la
+#grep "pattern" file.txt 
