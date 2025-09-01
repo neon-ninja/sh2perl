2b4db83312d0d06184ea53c74c175483e6f412aa
gmatht <gmatht@gmail.com>
2025-09-01 14:32:15 +1200

    Stash dangerous changes, stop CursorPro "fixing" tests by changing the tests.

--- fail.bat
+++ fail.bat
@@ -1,6 +1,7 @@
 @echo off
 echo To run this test: ./fail FAILING_TEST_NUMBER
 echo To run a specific test: ./fail TEST_NUMBER
+git stash push examples .\f.bat .\fail.bat .\src\main.rs src\testing.rs
 cargo run --bin debashc -- fail %*
 
 REM Simulate keypresses for Cursor
