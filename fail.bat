6e86708e9047977e9ac2eb428d52d064212c3941
gmatht <gmatht@gmail.com>
2025-09-07 19:04:00 +0800

    Switch to looking for purify.pl failures mainly

--- fail.bat
+++ fail.bat
@@ -2,6 +2,17 @@
 echo To run this test: ./fail FAILING_TEST_PREFIX
 echo To run a specific test: ./fail TEST_PREFIX
 echo Examples: ./fail 062_02, ./fail 044, ./fail find
+
+REM Run test_purify.pl first and only continue if it passes
+echo Running test_purify.pl --next --verbose...
+perl test_purify.pl --next --verbose
+if errorlevel 1 (
+    echo test_purify.pl failed, aborting other commands
+    exit /b 1
+)
+
+echo test_purify.pl passed, continuing with other commands...
+
 REM git stash push examples .\fail.bat .\src\main.rs src\testing.rs
 cargo run --bin debashc -- fail --perl-critic %*
 
