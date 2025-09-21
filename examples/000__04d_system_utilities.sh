74603b6d9d34bdd95150fce8f2a2350806dbecd6
gmatht <gmatht@gmail.com>
2025-09-21 23:25:59 +0800

    Reached 000__04d_system_utilities:y00001

--- examples/000__04d_system_utilities.sh
+++ examples/000__04d_system_utilities.sh
@@ -25,8 +25,8 @@ sleep $sleep_duration
 
 # which command with backticks
 #PERL_MUST_NOT_CONTAIN `which
-bash_path=`which bash`
-echo "Bash path: $bash_path"
+#bash_path=`which bash`
+#echo "Bash path: $bash_path"
 
 # yes command with backticks
 #PERL_MUST_NOT_CONTAIN `yes
