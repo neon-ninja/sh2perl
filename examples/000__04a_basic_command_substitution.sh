4f3d26df9935e03e679fa662bf1a1580aa0fee57
gmatht <gmatht@gmail.com>
2025-09-21 10:53:40 +0800

    Reached 000__04a_basic_command_substitution:y00002

--- examples/000__04a_basic_command_substitution.sh
+++ examples/000__04a_basic_command_substitution.sh
@@ -8,12 +8,12 @@ echo "=== Basic Command Substitution ==="
 # Simple command substitution
 echo "Current date: `date +%Y`"
 #echo "Current user: `whoami`"
-echo "Current directory: `pwd | basename`"
+echo "Current directory: `pwd | sed 's|.*/||'`"
 
 # Assigning backtick results to variables
 current_date=`date +%Y%m`
 #current_user=`whoami`
-current_dir=`pwd | basename`
+current_dir=`pwd | sed 's|.*/||'`
 
 echo "Stored date: $current_date"
 #echo "Stored user: $current_user"
